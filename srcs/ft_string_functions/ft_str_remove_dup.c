d7ffc3e239e7302dcc53ffe67052ddd02af3a57a
Gabriel Costa Matos <gcosta-m@c1r4p3.42sp.org.br>
2025-01-15 10:12:43 -0300

    remove_dup errada pra variar

--- srcs/ft_string_functions/ft_str_remove_dup.c
+++ srcs/ft_string_functions/ft_str_remove_dup.c
@@ -1,30 +1,29 @@
 #include "../includes/ft_string_functions.h"
 
-char *ft_str_remove_dup(char *str, char c)
+char	*ft_str_remove_dup(char *str, char c)
 {
-    int i;
-    int l;
-    char buffer[1024];
-    char *ptr;
+    int		i = 0;
+    int		l = 0;
+    char	*buffer;
+    char	*ptr;
 
     if (!str)
         return (NULL);
-    i = 0;
-    l = 0;
+    buffer = malloc((ft_strlen(str) + 1) * sizeof(char));
+    if (!buffer)
+        return (NULL);
     while (str[i])
     {
-        if (str[i] != c)
-            buffer[l++] = str[i++];
-        else if (str[i] == c)
-        {
-            buffer[l++] = str[i++];
+        buffer[l++] = str[i++];
+        if (str[i - 1] == c)
             while (str[i] == c)
                 i++;
-        }
     }
+    buffer[l] = '\0';
     ptr = malloc((l + 1) * sizeof(char));
     if (!ptr)
-        return (NULL);
+        return (free(buffer), NULL);
     ft_strlcpy(ptr, buffer, l + 1);
+    free(buffer);
     return (ptr);
 }
