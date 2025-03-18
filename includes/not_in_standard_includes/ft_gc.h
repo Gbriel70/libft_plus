23f5e5e96862c223e1d3a0fc1f5ff08d512319fa
Gabriel Costa Matos <gcosta-m@c1r5p2.42sp.org.br>
2025-03-18 10:04:44 -0300

    norm

--- includes/not_in_standard_includes/ft_gc.h
+++ includes/not_in_standard_includes/ft_gc.h
@@ -1,3 +1,15 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_gc.h                                            :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: gcosta-m <gcosta-m@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/03/18 10:02:26 by gcosta-m          #+#    #+#             */
+/*   Updated: 2025/03/18 10:02:28 by gcosta-m         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 #ifndef FT_GC_H
 # define FT_GC_H
 
@@ -8,13 +20,13 @@
 
 typedef struct s_gc
 {
-    void *ptr;
-    struct s_gc *next;
-} t_gc;
+	void		*ptr;
+	struct s_gc	*next;
+}				t_gc;
 
-void *gc_malloc(size_t size);
-void gc_free(void *ptr);
-void gc_exit(void);
-short gc_add(void *ptr);
+void			*gc_malloc(size_t size);
+void			gc_free(void *ptr);
+void			gc_exit(void);
+short			gc_add(void *ptr);
 
 #endif
