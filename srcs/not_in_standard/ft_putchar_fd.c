1c2b133a5e330ec0b32d7f24d8713a90b33bbe37
Gabriel Costa Matos <gcosta-m@c1r7p3.42sp.org.br>
2025-06-16 15:36:54 -0300

    sub update

--- srcs/not_in_standard/ft_putchar_fd.c
+++ srcs/not_in_standard/ft_putchar_fd.c
@@ -6,7 +6,7 @@
 /*   By: gcosta-m <gcosta-m@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/15 13:47:04 by gcosta-m          #+#    #+#             */
-/*   Updated: 2025/05/19 13:31:14 by gcosta-m         ###   ########.fr       */
+/*   Updated: 2025/06/16 15:35:57 by gcosta-m         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,6 +16,6 @@ void	ft_putchar_fd(char c, int fd);
 
 void	ft_putchar_fd(char c, int fd)
 {
-	if(write(fd, &c, 1) == -1)
+	if (write(fd, &c, 1) == -1)
 		return ;
 }
